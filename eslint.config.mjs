236b00f6ad27fc59ff270564eb6e845ee6ce0b38
Felipe-Belotto <felipeeduardo817@gmail.com>
2025-05-04 02:47:15 -0300

    feat: eslint config and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
-export default eslintConfig;
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+
+    rules: {
+      // Regras do ESLint devem ter um nível de severidade: "off" (0), "warn" (1), ou "error" (2)
+
+      "no-unused-vars": "error", // Use "error" em vez de uma mensagem
+    },
+  },
+]
+
+export default eslintConfig
